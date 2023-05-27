f8092e5f3b17dc68dcf5a453d40884ffa0d162a9
Moritaka <Moritaka@DESKTOP-9J3G886>
2023-05-27 23:35:40 +0700

    Update new Main.xaml

---
+++ View/Main.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PetStoreManagementApp.View
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Lấy nút Button đã được bấm
+            Button clickedButton = (Button)sender;
+
+            // Đặt nền của nút Button đã được bấm là không hiển thị (Collapsed)
+            clickedButton.Background = Brushes.Transparent;
+
+            // Lặp qua tất cả các nút Button
+            foreach (Button button in FindVisualChildren<Button>(this))
+            {
+                // Kiểm tra nếu nút Button không phải là nút đã được bấm
+                if (button != clickedButton)
+                {
+                    // Đặt nền của các nút còn lại là không hiển thị (Collapsed)
+                    button.Background = Brushes.Transparent;
+                }
+            }
+        }
+
+        // Phương thức hỗ trợ để tìm các phần tử con kiểu T trong một phần tử chính
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject dependencyObject) where T : DependencyObject
+        {
+            if (dependencyObject != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(dependencyObject, i);
+
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+    }
+}
